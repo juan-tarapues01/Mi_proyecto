233d5e70cf8a96f1da2140c4d83b1163e904eda0
Juan Tarapues <jftaerapuesch24@gmail.com>
2025-12-01 14:10:32 -0500

    Senaaaa

--- sql.sql
+++ sql.sql
@@ -4,5 +4,4 @@ CREATE TABLE IF NOT EXISTS contactos (
       correo VARCHAR(100) NOT NULL,       
       mensaje TEXT,                       
       creado TIMESTAMP DEFAULT NOW()     
-   );
-   """)
+);
